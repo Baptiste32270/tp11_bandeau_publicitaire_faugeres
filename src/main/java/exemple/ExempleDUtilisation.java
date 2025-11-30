3d19db2538bd24974599de9050fed687f4a2149c
Baptiste32270 <baptiste.faugeres@sfr.fr>
2025-11-30 11:34:43 +0100

    ajout diagramme de classes

--- src/main/java/exemple/ExempleDUtilisation.java
+++ src/main/java/exemple/ExempleDUtilisation.java
@@ -8,30 +8,30 @@ import bandeau.Bandeau;
 public class ExempleDUtilisation {
 
     public static void main(String[] args) {
-        // 1. Initialisation du bandeau
+        //Initialisation du bandeau
         Bandeau monBandeau = new Bandeau();
         Font fontBase = new Font("SansSerif", Font.BOLD, 24);
         monBandeau.setFont(fontBase);
         monBandeau.setForeground(Color.BLACK);
         
-        // 2. Création du scénario
+        //Création du scénario
         Scenario s = new Scenario();
         
         
-        // Étape 1 : Le texte s'écrit comme une machine (1 fois)
+        //Le texte s'écrit comme une machine (1 fois)
         monBandeau.setMessage("Initialisation du système...");
         s.ajouterEffet(new EffetMachineAEcrire(), 1);
         
-        // Étape 2 : Le texte "Vague" ondule (2 fois pour bien voir le mouvement)
+        //Le texte "Vague" ondule (2 fois)
         s.ajouterEffet(new EffetVague(), 2);
         
-        // Étape 3 : Effet d'étirement (1 fois)
+        //Effet d'étirement (1 fois)
         s.ajouterEffet(new EffetEtirement(), 1);
         
-        // Étape 4 : Zoom pour attirer l'attention (3 petits zooms ou 1 gros selon votre implémentation de Zoom)
+        //Zoom
         s.ajouterEffet(new EffetZoom(), 1);
         
-        // Étape 5 : Fondu au noir/blanc à la fin (1 fois)
+        //Fondu au noir/blanc (1 fois)
         s.ajouterEffet(new EffetAssombrissement(), 1);
         
         
